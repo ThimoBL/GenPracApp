c93d328dd8be207fa5add3e330949215b35ff588
thimobl <thimo5365@gmail.com>
2026-01-11 20:41:30 +0100

    test removing cors

--- GenPracApp.WebAPI/Program.cs
+++ GenPracApp.WebAPI/Program.cs
@@ -8,9 +8,9 @@ namespace GenPracApp.WebAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            var corsOrigin = builder.Configuration
-                .GetSection("Cors:AllowedOrigins")
-                .Get<string[]>();
+            // var corsOrigin = builder.Configuration
+            //     .GetSection("Cors:AllowedOrigins")
+            //     .Get<string[]>();
 
             builder.Services.AddSwaggerGen();
             builder.Services.AddOpenTelemetry().UseAzureMonitor();
@@ -20,16 +20,16 @@ namespace GenPracApp.WebAPI
                 .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
 
             // Add CORS to allow React SPA to call the API
-            builder.Services.AddCors(options =>
-            {
-                options.AddPolicy("AllowReactApp", policy =>
-                {
-                    policy.WithOrigins("https://gentle-mud-043be6803.3.azurestaticapps.net") // Your React app URL
-                          .AllowAnyHeader()
-                          .AllowAnyMethod()
-                          .AllowCredentials();
-                });
-            });
+            // builder.Services.AddCors(options =>
+            // {
+            //     options.AddPolicy("AllowReactApp", policy =>
+            //     {
+            //         policy.WithOrigins("https://gentle-mud-043be6803.3.azurestaticapps.net") // Your React app URL
+            //               .AllowAnyHeader()
+            //               .AllowAnyMethod()
+            //               .AllowCredentials();
+            //     });
+            // });
 
             // Add services to the container.
 
