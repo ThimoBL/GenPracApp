ace99a788dbff35cd7d0d3bca9bdd9105d95f27c
thimobl <thimo5365@gmail.com>
2026-01-11 18:17:20 +0100

    Minor frontend tweaks for env variables

--- genpracapp.webspa/vite.config.ts
+++ genpracapp.webspa/vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      // Remove the `external` option or only use it if you have a specific reason.
+    }
+  },
+  define: {
+    'process.env': process.env
+  }
 })
